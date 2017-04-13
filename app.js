79c586e5da284c680baa1bdf232f73d8f0f1ca35
denkomanceski <razerqwe123>
2017-04-13 14:10:24 +0200

    exercise #2

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 /**
  * Created by denkomanceski on 4/13/17.
  */
+import {today} from "./src/today";
+import {tomorrow} from "./src/tomorrow";
+
+today();
+tomorrow();
 console.log("Hello World");
---
+++ src/today.js
@@ -0,0 +1,6 @@
+/**
+ * Created by denkomanceski on 4/13/17.
+ */
+export const today = () => {
+    console.log("Today");
+};
